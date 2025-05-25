f83837844cf9939fb96b9f2f555d02233ce9d7e5
Vignesh-k-arc <vigensh.k@helixsense.com>
2025-05-25 19:46:49 +0530

    hh

---
+++ images/sfx/dark-images/icons/fontAwesomeIcons.js
@@ -0,0 +1,11 @@
+import { faBell } from '@fortawesome/free-solid-svg-icons/faBell';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons/faCheckCircle';
+import { faPhoneAlt } from '@fortawesome/free-solid-svg-icons/faPhoneAlt';
+import { faPlusCircle } from '@fortawesome/free-solid-svg-icons/faPlusCircle';
+
+export {
+  faBell,
+  faCheckCircle,
+  faPhoneAlt,
+  faPlusCircle,
+};
